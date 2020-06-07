56ca2b72196f748b95e4a87e5025bc6a888685d9
Amr Elnaggar <amr.eln2gar@gmail.com>
2020-06-08 00:32:19 +0200

    Problem 1.2: Fixing the js code

--- chapter1/javascript/checkPermutation.js
+++ chapter1/javascript/checkPermutation.js
@@ -1,7 +1,7 @@
 let checkPermutation = (str1, str2) => {
-    str1.split('').sort().join('');
-    str2.split('').sort().join('');
-    return str1 === str2;
+    sorted_str1 = str1.split('').sort().join('');
+    sorted_str2 = str2.split('').sort().join('');
+    return sorted_str1 === sorted_str2;
 }
 
 // This solution is O(nlog(n)) in time, O(n) space
